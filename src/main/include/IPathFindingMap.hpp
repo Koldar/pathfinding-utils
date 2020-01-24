e010ecd2ee7daea6b684d2c147bf891c3791caa4
Massimo Bono <massimobono1@gmail.com>
2020-01-24 17:21:47 +0100

    update doc

--- src/main/include/IPathFindingMap.hpp
+++ src/main/include/IPathFindingMap.hpp
@@ -3,30 +3,34 @@
 
 #include <string>
 
+#include <cpp-utils/IImageable.hpp>
+
 namespace pathfinding::maps {
 
-/**
- * @brief represents a map where the agents traverse in order to reach an objective
- * 
- * @tparam G 
- * @tparam V 
- * @tparam E 
- */
-class IPathFindingMap {
-public:
+    using namespace cpp_utils;
+
     /**
-     * @brief the name of the map
+     * @brief represents a map where the agents traverse in order to reach an objective
      * 
-     * @return const std::string& map name
+     * @tparam G 
+     * @tparam V 
+     * @tparam E 
      */
-    virtual const std::string& getName() const = 0;
-    /**
-     * @brief number of available locations the map has
-     * 
-     * @return size_t the number of locations an agent can traverse
-     */
-    virtual size_t getSize() const = 0;
-};
+    class IPathFindingMap: public IImageable {
+    public:
+        /**
+         * @brief the name of the map
+         * 
+         * @return const std::string& map name
+         */
+        virtual const std::string& getName() const = 0;
+        /**
+         * @brief number of available locations the map has
+         * 
+         * @return size_t the number of locations an agent can traverse
+         */
+        virtual size_t getSize() const = 0;
+    };
 
 }
 
